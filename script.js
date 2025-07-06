87047cdf8ff7873dca7217cb49c901545a5d6d0b
VIVEK <viveksahay18@gmail.com>
2025-07-06 11:00:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function addTask() {
+  const taskInput = document.getElementById("taskInput");
+  const taskText = taskInput.value.trim();
+
+  if (taskText === "") {
+    alert("Please enter a task.");
+    return;
+  }
+
+  const li = document.createElement("li");
+
+  li.textContent = taskText;
+  li.addEventListener("click", function () {
+    li.classList.toggle("completed");
+  });
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "Delete";
+  deleteBtn.onclick = function () {
+    li.remove();
+  };
+
+  li.appendChild(deleteBtn);
+
+  document.getElementById("taskList").appendChild(li);
+  taskInput.value = "";
+}
